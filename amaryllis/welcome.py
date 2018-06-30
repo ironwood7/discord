fa3343d837cd28c0cefb40759d32f4b8b5d69e11
ironwood7 <shamon.zdc@gmail.com>
2018-06-30 15:36:29 +0900

    rep welcome name -> mention ret

--- amaryllis/welcome.py
+++ amaryllis/welcome.py
@@ -3,13 +3,13 @@ import asyncio
 import random
 
 msg_welcome=[
-    "いらっしゃいませ @{} 様",
-    "ようこそ @{} 様",
+    "いらっしゃいませ {} 様",
+    "ようこそ {} 様",
         ]
 
 msg_remove=[
-    "またいらしてください @{} 様",
-    "いってらっしゃいませ @{} 様",
+    "またいらしてください {} 様",
+    "いってらっしゃいませ {} 様",
         ]
 
 
@@ -18,12 +18,13 @@ def on_ready():
     return
 
 async def on_member_join_inner(client, member, channel_id):
+    username = member
     chObj = client.get_channel(channel_id)
-    await client.send_message(chObj, random.choice(msg_welcome).format(member))
+    await client.send_message(chObj, random.choice(msg_welcome).format(member.mention))
 
 async def on_member_remove_inner(client, member, channel_id):
     chObj = client.get_channel(channel_id)
-    await client.send_message(chObj, random.choice(msg_remove).format(member))
+    await client.send_message(chObj, random.choice(msg_remove).format(member.mention))
 
 
 
